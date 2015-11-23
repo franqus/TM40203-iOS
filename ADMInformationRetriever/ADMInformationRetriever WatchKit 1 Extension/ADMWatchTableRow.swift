c1a2e0ce40c48ceb0fd13fbc8ac646ad32be19bc
panzerflorian <florianpanzer@Florians-MacBook-Air.local>
2015-11-23 22:01:27 +0100

    display results on watch

---
+++ ADMInformationRetriever/ADMInformationRetriever WatchKit 1 Extension/ADMWatchTableRow.swift
@@ -0,0 +1,23 @@
+//
+//  ADMWatchTableRow.swift
+//  ADMInformationRetriever
+//
+//  Created by Florian Panzer on 23.11.15.
+//  Copyright © 2015 sovanta AG. All rights reserved.
+//
+
+//import UIKit
+import WatchKit
+
+class ADMWatchTableRow: NSObject {
+
+    @IBOutlet var group : WKInterfaceGroup!
+    @IBOutlet var label : WKInterfaceLabel!
+    var document : ADMDocument!
+    
+    func assignDocument(document : ADMDocument){
+        self.document = document
+        label.setAttributedText(NSAttributedString(string: self.document.title))
+    }
+
+}
--- ADMInformationRetriever/ADMInformationRetriever WatchKit 1 Extension/InterfaceController.swift
+++ ADMInformationRetriever/ADMInformationRetriever WatchKit 1 Extension/InterfaceController.swift
@@ -12,10 +12,64 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var table : WKInterfaceTable!
+    let documentsPerSection: String = "10"
+    var manager: ADMInformationManager!
+    var results: Array<ADMDocument> = [ADMDocument]()
+    var totalResults: Int = 0
+
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        self.manager = ADMInformationManager(mockFilename: "mockdata")
+        self.manager.curServer = self.manager.servers[0]
+        
+        let searchtext = "test"
+        let paramDict: NSDictionary =
+               ["length":self.documentsPerSection,
+                "startIndex":"0",
+                "search":["journal":searchtext,
+                    "authors":searchtext,
+                    "title":searchtext,
+                    "institutions":searchtext,
+                    "abstract":searchtext]]
+        
+        let query: ADMQuery = ADMQuery.init(params: paramDict)
+        self.manager.sendQuery(query, server: self.manager.curServer, index: 0, length: Int(self.documentsPerSection)!) { (response, totalResults, error) -> Void in
+            
+            if(error == nil)
+            {
+                self.results = response as! Array<ADMDocument>
+                self.totalResults = totalResults
+                self.receiveResults()
+                
+                dispatch_async(dispatch_get_main_queue())
+                    {
+                        print("hhh")
+                        //self.aiRequest.stopAnimating()
+                        //self.btnGo.hidden = false
+                        //self.tvResults.reloadData()
+                }
+            }
+            else
+            {
+                dispatch_async(dispatch_get_main_queue())
+                    {
+                        //self.aiRequest.stopAnimating()
+                        //self.btnGo.hidden = false
+                }
+                print("error=%@",error)
+            }
+        }
+
+    }
+    
+    func receiveResults(){
+        self.table.setNumberOfRows(results.count,withRowType: "ADMWatchTableRow")
+        for(var i=0; i<results.count; i++){
+            let row : ADMWatchTableRow = self.table.rowControllerAtIndex(i) as! ADMWatchTableRow
+            row.assignDocument(results[i])
+        }
     }
 
     override func willActivate() {
@@ -27,5 +81,5 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
 }
